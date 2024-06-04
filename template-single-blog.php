2e5b659c041927383a67ab6aa93bf0b2812d1f15
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-06-04 11:52:23 +0545

    test

--- template-single-blog.php
+++ template-single-blog.php
@@ -6,7 +6,7 @@ get_header();
 	<main id="primary" class="site-main container">
 <div class="section-block"></div>
 <i class="bi bi-chevron-left"></i>Back
-   <div class="title-block e5">
+   <div class="title-block e6">
 	<button class='btn'>Informational</button>
 	<h1>The Importance of FMC's</h1>
 	<p>29 April 2024 <i class="bi bi-dot"></i> 08:02 AM</p>
@@ -30,6 +30,7 @@ get_header();
     </div>
   </div>
   <div class="col-8">
+	<h1>test</h1>
     <div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
 	<p>In the bustling world of healthcare, there's an often-overlooked group of professionals whose work plays a critical role in understanding and analyzing complex medical events: forensic medical coders. While most people are familiar with medical coders in a clinical setting, forensic medical coders operate in a different arena, often working with law enforcement, insurance companies, and legal teams. Let's dive into their world to understand what they do and why their work is so important.</p>
       <h4 id="sec1">What Are Forensic Medical Coders?</h4>

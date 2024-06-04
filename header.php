b61526f059483ab7bbdba8b34496a3059779734d
Subham Shrestha <subhamshrestha@Subhams-MacBook-Pro.local>
2024-06-04 12:53:43 -0700

    reponsive fixed and separate responsive file

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/src/css/responsive.css">
 	<?php wp_head(); ?>
 	
 </head>

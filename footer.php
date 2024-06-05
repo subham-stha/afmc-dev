d0ac541fa87318a95799e81b379d66e5f45df9db
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-06-05 18:00:16 +0545

    responsiveness sytle set up

--- footer.php
+++ footer.php
@@ -45,8 +45,6 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<script src="<?php echo get_template_directory_uri();?>/san.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></scrip>
-
 </body>
 </html>
